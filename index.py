59aa75779aacc16a4bce6be41104f6e83a95aa95
neusj47 <neusj47@gmail.com>
2021-11-17 00:56:46 +0900

    # commit

--- index.py
+++ index.py
@@ -2,26 +2,50 @@
 # 0. 대상 코드 입력
 # 1. 맵핑 정보 가져오기
 # 2. 지수생성 (동일가중)
+# 3. 모멘텀시그널
+# 4. 수익률 산출
 
 import pandas as pd
 import requests
 import re
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+import warnings
+warnings.filterwarnings(action='ignore')
 
-start_date = '20201114'
+start_date = '20191114'
 end_date = '20211114'
-
+selected_num = 2
+lookback_m = 1
 df_krx = fdr.StockListing('KRX')
+
+bm = pd.DataFrame(fdr.DataReader('148020', start_date, end_date)['Close'])
 
 
 # 0. 대상 코드 입력
 thema_info = {496: '골프',
               492: 'NFT',
-              504: '요소수 관련주',
+              503: 'LFP/리튬인산철',
               44 : '시멘트/레미콘',
-              488 : '웹툰'}
-
+              206 : '농업',
+              42 :  '게임',
+              488 : '웹툰',
+              434 : '재택근무/스마트워크',
+              498 : 'CMO(의약품 위탁생산)',
+              480 : '메타버스(Metaverse)',
+              265 : '모바일게임(스마트폰)',
+              331 : '가상화폐(비트코인 등)',
+              310 : '리츠(REITs)',
+              232 : '미디어(방송/신문)',
+              387 : '블록체인',
+              500 : '폐배터리',
+              386 : '수소차(연료전지/부품/충전소 등)',
+              152 : '온실가스(탄소배출권)',
+              205 : '원자력발전',
+              128 : '엔터테인먼트'
+             }
 
 # 1. 맵핑 정보 가져오기
 def get_thema_code(num) :
@@ -50,11 +74,52 @@ def get_thema_idx(thema_info, df_krx) :
         df = df.pct_change().fillna(0)
         idx_temp = pd.DataFrame(df.sum(axis=1)/len(df.columns), index = df.index, columns= [value])
         idx = pd.concat([idx,idx_temp], axis =1)
-        cum_idx = (1 + idx).cumprod() - 1
-    return cum_idx
-cum_idx = get_thema_idx(thema_info, df_krx)
+    cum_idx = (1 + idx).cumprod() - 1
+    return cum_idx, idx
 
+cum_idx, idx = get_thema_idx(thema_info, df_krx)
 
+# 3. 모멘텀 시그널 생성
+def get_rm_signal(cum_idx, lookback_m, selected_num) :
+    month_list = cum_idx.index.map(lambda x : datetime.datetime.strftime(x, '%Y-%m')).unique()
+    rebal_date= pd.DataFrame()
+    for m in month_list:
+        rebal_date = rebal_date.append(cum_idx[cum_idx.index.map(lambda x : datetime.datetime.strftime(x, '%Y-%m')) == m].iloc[-1])
+    rebal_date = rebal_date - rebal_date.shift(lookback_m).fillna(0)
+    signal_m = pd.DataFrame((rebal_date.rank(axis=1, ascending = False) <= selected_num).applymap(lambda x : '1' if x == True else '0'))
+    signal_m = signal_m.shift(1).fillna(0)
+    return signal_m
 
-plt.plot(cum_idx,label = [i for i in cum_idx.columns])
+signal_m = get_rm_signal(cum_idx, lookback_m, selected_num)
+
+# 4. 수익률 산출
+def get_rm_return(idx,signal_m) :
+    idx = idx.rename_axis('Date').reset_index()
+    idx['Date'] = pd.to_datetime(idx['Date'])
+    idx['YYYY-MM'] = idx['Date'].map(lambda x : datetime.datetime.strftime(x, '%Y-%m'))
+    signal_m['YYYY-MM'] =signal_m.index.map(lambda x : datetime.datetime.strftime(x, '%Y-%m'))
+    signal_d = pd.merge(idx[['Date','YYYY-MM']], signal_m, on = 'YYYY-MM', how = 'left')
+    signal_d.set_index(['Date'],inplace=True)
+    signal_d = signal_d[thema_info.values()].astype(float)
+    idx.set_index(['Date'],inplace=True)
+    result = pd.DataFrame(((signal_d * idx) * 1 / selected_num).sum(axis=1))
+    return result, signal_d
+
+result = get_rm_return(idx,signal_m)[0]
+signal_d = get_rm_return(idx,signal_m)[1]
+
+# Cumulative Compounded Returns for Momentum
+plt.figure(figsize=(17,7))
+plt.title('Relative Momentum Return')
+plt.plot((1 + result).cumprod() - 1, label = 'Momentum')
+plt.plot((1 + bm['Close'].pct_change().fillna(0)).cumprod() - 1, label = 'BenchMark')
 plt.legend()
+plt.show()
+
+# Cross-Sectional Weights
+plt.figure(figsize=(17,7))
+plt.title('Cross-Sectional Weights')
+plt.stackplot(signal_d.index, np.transpose(signal_d),labels = signal_d.columns)
+plt.legend()
+plt.show()
+
